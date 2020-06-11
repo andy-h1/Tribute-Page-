dd72b7b6302f381dc2bad8791aad27e1d1d3f9b7
Andy Huynh <a.huynh2612@gmail.com>
2020-06-11 12:34:35 +0100

    Added Github details to modal

--- tribute.js
+++ tribute.js
@@ -1,39 +1,36 @@
 const cardButtons = document.querySelectorAll('.card button');
 const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
-const endpoint = 'https://api.github.com/users/d-pagey';
-const userEl = document.querySelector('.user');
-
-const danPromise = fetch(endpoint);
-danPromise.then(response => {
-  return response.json();
-}).then(data => {
-  console.log(data);
-  console.log(data.name);
-  console.log(data.blog);
-  console.log(data.company);
-  userEl.textContent = `${data.name} ${data.company}`;
-}).catch(handleError)
+const baseEndpoint = 'https://api.github.com/users';
 
 function handleError(err) {
   console.log("ERROR!");
   console.log(err);
 }
 
+
 function handleCardButtonClick(event) {
-  console.log('CLICK');
   const button = event.currentTarget;
   const card = button.closest('.card');
   const imgDataSrc = card.dataset.alt;
-  console.log(imgDataSrc);
   const desc = card.dataset.description;
-  console.log(desc);
-  const name = card.querySelector('h2').textContent;
+  const githubName = card.dataset.github;
+  const githubPromise = fetch(`${baseEndpoint}/${githubName}`);
+  console.log(githubPromise);
+  githubPromise.then(response => {
+    return response.json();
+  }).then(data => {
+    const name = card.querySelector('h2').textContent;
   modalInner.innerHTML = `
     <img width="600" height="600" src="${imgDataSrc}" alt="${name}"/>
     <p>${desc}</p>
+    <ul>
+    <li>${data.company}</li>
+    <li>${data.html_url}
+    </ul>
     `;
   modalOuter.classList.add('open');
+  }).catch(handleError)
 }
 
 cardButtons.forEach(button =>
